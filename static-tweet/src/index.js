bfa20f190f2dcadb4430e36c8bf70fb7ee8d5f17
prateek-patel <patel.prateek26@gmail.com>
2018-11-25 16:29:10 +0530

    initial commit for static tweet

---
+++ static-tweet/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Tweet() {
+    return (
+        <div className="tweet">Tweet</div>
+    )
+}
+
+ReactDOM.render(<Tweet/>, document.querySelector('#root'));
